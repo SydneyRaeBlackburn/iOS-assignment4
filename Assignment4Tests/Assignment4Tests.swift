1dd2bf53db77341039a3e0bde0fcaf7d734e0727
Sydney Blackburn <sydneyblackburn10@gmail.com>
2018-10-25 17:03:06 -0700

    Initial Commit

---
+++ Assignment4Tests/Assignment4Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Assignment4Tests.swift
+//  Assignment4Tests
+//
+//  Created by Sydney Blackburn on 10/25/18.
+//  Copyright © 2018 Sydney Blackburn. All rights reserved.
+//
+
+import XCTest
+@testable import Assignment4
+
+class Assignment4Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
